3b9c87dc95568796a0887cdac5f7cafe425f615e
dizzyvduijn <112895782+dizzyvduijn@users.noreply.github.com>
2022-11-28 19:37:17 +0100

    Update question1.py

--- assignment1/question1.py
+++ assignment1/question1.py
@@ -12,13 +12,15 @@ mnist_data = pd.read_csv('mnist.csv').values
 labels = mnist_data[:, 0]
 digits = mnist_data[:, 1:]
 img_size = 28
-# plt.imshow(digits[0].reshape(img_size, img_size))
-# plt.show()
+
+for i in range(3):
+    plt.imshow(digits[i].reshape(img_size, img_size))
+    plt.show()
 
 
 """ Point 1: exploratory analysis"""
 # describe can be used only on a dataFrame not on array 
-print(pd.DataFrame(mnist_data).describe())
+# print(pd.DataFrame(mnist_data).describe())
 # # we can eliminate those variables where min and max coincide, because it means that for all observation that pixel is the same
 
 # # Distirbution of digits: histogram
@@ -27,14 +29,14 @@ print(pd.DataFrame(mnist_data).describe())
 
 
 # # Frequency table
-occurency_table = np.empty(shape=(10, 3), dtype=object)
+# occurency_table = np.empty(shape=(10, 3), dtype=object)
 
-for i in range(len(np.unique(labels))):
-    count = np.count_nonzero(labels==i)
-    percentage = (count/len(digits)*100)
-    occurency_table[i]= int(i), int(count), round(float(percentage),2)
+# for i in range(len(np.unique(labels))):
+#     count = np.count_nonzero(labels==i)
+#     percentage = (count/len(digits)*100)
+#     occurency_table[i]= int(i), int(count), round(float(percentage),2)
 
-print(occurency_table)
+# print(occurency_table)
 
 
 # # Digits are quite evenly distibuted, meaning that predicting the majority class will leave us with a low succes rate
