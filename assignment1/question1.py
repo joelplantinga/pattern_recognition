2c62568b4a78480d4994deb61e6a276c5bf89596
dizzyvduijn <112895782+dizzyvduijn@users.noreply.github.com>
2022-11-28 15:12:04 +0100

    Update question1.py

--- assignment1/question1.py
+++ assignment1/question1.py
@@ -21,8 +21,24 @@ img_size = 28
 print(pd.DataFrame(mnist_data).describe())
 # # we can eliminate those variables where min and max coincide, because it means that for all observation that pixel is the same
 
+# # Distirbution of digits: histogram
+# plt.hist(labels, bins=10) 
+# plt.show() 
 
-plt.hist(labels, bins=10) 
-plt.show() 
+
+# # Frequency table
+occurency_table = np.empty(shape=(10, 3), dtype=object)
+
+for i in range(len(np.unique(labels))):
+    count = np.count_nonzero(labels==i)
+    percentage = (count/len(digits)*100)
+    occurency_table[i]= int(i), int(count), round(float(percentage),2)
+
+print(occurency_table)
+
+
+# # Digits are quite evenly distibuted, meaning that predicting the majority class will leave us with a low succes rate
+
 print(f'Percentage of correct predictions assigning all to class 1: {len(labels[labels==1])/len(labels)}')
 # evenly distributed 
+
